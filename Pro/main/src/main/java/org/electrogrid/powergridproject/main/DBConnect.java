1186e0acbeffaa4aeb35015a9d510a782fb49bde
o_weerasinghe <oshanweerasinghe28@gmail.com>
2022-04-23 13:26:42 +0530

    dbAdded

---
+++ Pro/main/src/main/java/org/electrogrid/powergridproject/main/DBConnect.java
@@ -0,0 +1,28 @@
+package org.electrogrid.powergridproject.main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnect {
+	
+	private static String url = "jdbc:mysql://localhost:3306/packages";
+	private static String user = "root";
+	private static String pass = "oshan@0765649523";
+	public static Connection con;
+	
+	public static Connection getConnection() {
+			
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+				
+			con = DriverManager.getConnection(url, user, pass);
+						
+		}
+		catch(Exception e) {
+			System.out.println("Database connection is not success!");
+		}
+			
+		return con;
+	}
+
+}
---
+++ Pro/main/src/main/java/org/electrogrid/powergridproject/main/Item.java
@@ -0,0 +1,5 @@
+package org.electrogrid.powergridproject.main;
+
+public class Item {
+
+}
